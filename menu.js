4785d9b6e24a45d877ea83425d11c922a91bcb17
anto <antocortez18d@gmail.com>
2025-10-23 10:34:38 -0300

    diario

--- menu.js
+++ menu.js
@@ -2,3 +2,7 @@ function toggleMenu() {
   const menu = document.getElementById("menu");
   menu.classList.toggle("menu-hidden");
 }
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("menu-hidden");
+}
