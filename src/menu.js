d42f9382d95f7f695b94f8ceb19557b191ea0f4f
Altamira <juanaaltamira7@gmail.com>
2025-11-29 13:58:46 -0300

    correcion errores cambios de carpeta, eliminacion de archivos sin utilidad y correccion detalles

--- src/menu.js
+++ src/menu.js
@@ -2,4 +2,12 @@ function toggleMenu() {
   const menu = document.getElementById("menu");
   menu.classList.toggle("menu-hidden");
 }
-
+function iniciarTour() {
+  introJs().setOptions({
+    steps: [
+      { intro: "Bienvenido al manual interactivo de ECO" },
+      { element: "#boton-menu", intro: "Acá abrís el menú principal" },
+      { element: "#boton-volver", intro: "Este es el botón para volver" }
+    ]
+  }).start();
+}
